cdfb25bf0bd1b9eb5ea294f0b9a45b9274771e0f
innovate.invent@gmail.com <ncm3@sfu.ca>
2017-08-22 16:58:58 -0700

    -Bug fixes -Fix coordinate sorting when trimming beginning of read

--- clip.py
+++ clip.py
@@ -4,9 +4,9 @@ if platform.python_implementation() == 'PyPy':
     import pypysam
 else:
     import pysam
-from sys import maxsize
+from sys import maxsize, stderr
 from CigarIterator import CigarIterator, appendOrInc
-import multiprocessing, ctypes
+import multiprocessing, ctypes, io
 import parapysam
 
 default_cost = {
@@ -93,7 +93,9 @@ def trimRecord(record: pysam.AlignedSegment, mate: pysam.AlignedSegment, start: 
 
     #Update record
     record.cigartuples = ops
-    if nextMatch is not None: record.reference_start = nextMatch
+    if nextMatch is not None:
+        record.set_tag('OS', record.reference_start)
+        record.reference_start = nextMatch
     record.mapping_quality = calculateMappingQuality(record)
     # TODO rewrite MD
     mate.next_reference_start = record.reference_start
@@ -183,8 +185,9 @@ def mergeRecord(fromRecord: pysam.AlignedSegment, toRecord: pysam.AlignedSegment
     # TODO Store tag for which strand the base originated from
 
 class WorkerProcess(parapysam.OrderedWorker):
-    def __init__(self, alternate):
+    def __init__(self, alternate, clipOnly):
         self.alternate = alternate
+        self.clipOnly = clipOnly
         super().__init__()
 
     def work(self):
@@ -201,11 +204,11 @@ class WorkerProcess(parapysam.OrderedWorker):
                     leftRecord = secondRecord
                 # alternate clipped read to avoid strand bias
                 if self.alternate and flipflop:
-                    mergeRecord(rightRecord, leftRecord)
+                    if not self.clipOnly: mergeRecord(rightRecord, leftRecord)
                     trimRecord(rightRecord, leftRecord, leftRecord.reference_end-1)
                     flipflop = False
                 else:
-                    mergeRecord(leftRecord, rightRecord)
+                    if not self.clipOnly: mergeRecord(leftRecord, rightRecord)
                     trimRecord(leftRecord, rightRecord, 0, rightRecord.reference_start)
                     flipflop = True
                 self.sendRecord(firstRecord)
@@ -264,9 +267,16 @@ class WriterProcess(parapysam.OrderedWorker):
                     self.nextIndex.value += 1
                     outFile.write(result[1])
                 else:
-                    if self.ordered == 'c' and self.nextIndex.value < result[0]:
-                        self.nextIndex.value = result[0]
-                    heapq.heappush(writeBuffer, HeapNode(result[0], result))
+                    try:
+                        startPos = result[1].get_tag('OS') #type: int
+                    except KeyError:
+                        startPos = result[1].reference_start
+                    if self.ordered == 'c':
+                        if self.nextIndex.value < startPos:
+                            self.nextIndex.value = startPos
+                        heapq.heappush(writeBuffer, HeapNode(startPos, result))
+                    else:
+                        heapq.heappush(writeBuffer, HeapNode(result[0], result))
                     self.bufferedCount.value += 1
                 while len(writeBuffer) and (writeBuffer[0].key + self.maxTLen < self.nextIndex.value) if self.ordered == 'c' else (writeBuffer[0].key == self.nextIndex.value):
                     result = heapq.heappop(writeBuffer)
@@ -287,16 +297,15 @@ class WriterProcess(parapysam.OrderedWorker):
                 self.nextIndex.value, record = p.receiveOrderedRecord()
                 outFile.write(record)
 
-def status(mateCount, bufferedCount, nextIndex):
+def status(mateCount, bufferedCount, nextIndex, logStream: io.IOBase = stderr):
     import time
-    lastIndex = nextIndex.value
+    logStream.write("\n") # This will be deleted by the next write
     while True:
-        status = "\x1b[2K\rMates buffered: {:>10}\tPending output: {:>10}\tRecords per second: ~{:>10}\tWaiting for read number: {:>10}".format(mateCount.value, bufferedCount.value, nextIndex.value - lastIndex, nextIndex.value)
-        stderr.write(status)
-        lastIndex = nextIndex.value
-        time.sleep(1)
+        status = "\x1b[F\x1b[2K\rMates buffered: {:>10}\tPending output: {:>10}\tWaiting for read number: {:>10}\n".format(mateCount.value, bufferedCount.value, nextIndex.value)
+        logStream.write(status)
+        time.sleep(0.2)
 
-def clip(inStream, outStream, threads = 8, maxTLen=1000, outFormat='bu', ordered=False, alternate=False, verbose=False):
+def clip(inStream: io.IOBase, outStream: io.IOBase, threads: int = 8, maxTLen: int = 1000, outFormat:str = 'bu', ordered=False, alternate=False, trimBarcode=False, clipOnly=False, verbose=False, logStream: io.IOBase=stderr):
     pool = []
     mateBuffer = {}
     inFile = pysam.AlignmentFile(inStream)
@@ -304,7 +313,7 @@ def clip(inStream, outStream, threads = 8, maxTLen=1000, outFormat='bu', ordered
 
     # Begin processing
     for _ in range(threads):
-        worker = WorkerProcess(alternate)
+        worker = WorkerProcess(alternate, clipOnly)
         worker.start(inFile.header)
         pool.append(worker)
 
@@ -319,7 +328,7 @@ def clip(inStream, outStream, threads = 8, maxTLen=1000, outFormat='bu', ordered
     poolLoop = poolLooper()
     mateCount = multiprocessing.RawValue(ctypes.c_long, 0)
 
-    statusProc = multiprocessing.Process(target=status, args=(mateCount, writer.bufferedCount, writer.nextIndex))
+    statusProc = multiprocessing.Process(target=status, args=(mateCount, writer.bufferedCount, writer.nextIndex, logStream))
     if verbose:
         statusProc.start()
 
@@ -346,15 +355,19 @@ def clip(inStream, outStream, threads = 8, maxTLen=1000, outFormat='bu', ordered
     writer.stop()
     if verbose:
         statusProc.terminate()
+        logStream.write("\x1b[F\x1b[2K\rCompleted.\n")
+    outStream.close()
 
 if __name__ == '__main__':
     import getopt, os, errno
-    from sys import stdout, stdin, stderr, argv
+    from sys import stdout, stdin, argv
     threads = 2
     maxTLen = 1000
     outFormat = ''
     ordered = False
     alternate = False
+    clipOnly = False
+    trimBarcode = False
     verbose = False
 
     #Command line options
@@ -371,6 +384,8 @@ if __name__ == '__main__':
                              "-t # Threads to use for processing (Default=1)\n"
                              "-m # Maximum template length guaranteeing no read overlap (Default=1000)\n"
                              "-a Alternate strand being clipped to avoid strand bias (RAM intensive)\n"
+                             "-b Trim trailing barcode region using BC tag"
+                             "-c Clip only, do not merge clipped region into mate\n"
                              "-o [sbuc] Output format: s=SAM (Default), b=BAM compressed, bu=BAM uncompressed, c=CRAM\n"
                              "-s Maintain input order (High depth regions may fill RAM), if not set will output in arbitrary order (Minimal RAM)\n"
                              "-v Verbose status output\n")
@@ -381,6 +396,10 @@ if __name__ == '__main__':
                 maxTLen = int(val or 1000)
             elif op == '-a':
                 alternate = True
+            elif op == '-c':
+                clipOnly = True
+            elif op == '-b':
+                trimBarcode = True
             elif op == '-o' and val != 's':
                 outFormat += val
             elif op == '-s':
@@ -395,4 +414,4 @@ if __name__ == '__main__':
             if e.errno != errno.EEXIST:
                 raise
 
-    clip(paths[0] if len(paths) else stdin, open(paths[1], 'wb+') if len(paths) > 1 else stdout, threads, maxTLen, outFormat, ordered, alternate, verbose)
+    clip(paths[0] if len(paths) else stdin, open(paths[1], 'wb+') if len(paths) > 1 else stdout, threads, maxTLen, outFormat, ordered, alternate, trimBarcode, verbose=verbose)
