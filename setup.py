0016468a37d3de7fb81e6a582200718dd98f11aa
innovate.invent@gmail.com <ncm3@sfu.ca>
2017-09-01 16:57:45 -0700

    Cleaned up some

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
 #! /usr/bin/env python
-
 from setuptools import setup
-import re
 
 # Imports version number
-VERSIONFILE = "ProDuSe/__version.py"
-verstrline = open(VERSIONFILE, "rt").read()
-verRegex = r"^__version__ = ['\"]([^'\"]*)['\"]"
-currentVer = re.search(verRegex, verstrline, re.M)
-if currentVer:
-	version = currentVer.group(1)
-else:
+try:
+    from ProDuSe.__version import __version__ as version
+except ImportError:
 	version = "Unknown"
 
 setup(
 	name="ProDuSe",
 	version=version,
 	description="Variant caller for semi-degenerate barcoded adapter libraries",
-	author="Nolan Martin",
-	author_email="ncm3@sfu.ca",
+	author="Nolan",
+	author_email="innovate.invent@gmail.com",
 	maintainer="Christopher Rushton",
 	maintainer_email="ckrushto@sfu.ca",
 	packages=["ProDuSe"],
@@ -26,9 +20,9 @@ setup(
 		"pysam",
 		"sortedcontainers",
 		"networkx",
-		"ruamel.yaml",
-		"jmespath",
-		"asciimatics"],
+		"configutator",
+		"clipoverlap",
+        "CigarIterator"],
 	license="GNU GPLv3",
 	scripts=["bin/produse"]
 	)
