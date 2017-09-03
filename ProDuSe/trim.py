7d2e6d677c8f0e2f6a99812736eb7accdd975dc8
Christopher Rushton <ckrushto@sfu.ca>
2017-09-02 22:58:50 -0700

    Bug fixes, optimized verbose output
    Fixed a bug caused when an output directory is not specified for the output file
    Fixed a bug causing the input files to be overwritten
    Fixed a bug caused by out of scope modules
    Fixed a bug caused by missing modules when running ProDuSe installed
    Modified verbose output to only update every 1000 records
    Changed verbose output to be consistent with the existing ProDuSe verbose output
    Verbose is enabled by default

--- ProDuSe/trim.py
+++ ProDuSe/trim.py
@@ -1,29 +1,40 @@
 #!/usr/bin/env python3
-import io, gzip
+import io
+import gzip
 from sys import stdin, stdout, stderr
-import FastqRecord
 from configutator import ConfigMap, ArgMap, PositionalArg, TransformCfg
+import time
+import os
+import errno
+
+# If called directly as a script
+try:
+    import FastqRecord
+# If installed
+except ImportError:
+    from ProDuSe import FastqRecord
 
 IUPACCodeDict = {
-    'A' : 'A',    #Adenine
-    'C'	: 'C',    #Cytosine
-    'G'	: 'G',    #Guanine
-    'T' : 'TU',    #Thymine
-    'U' : 'TU',    #Uracil
-    'R' : 'AG',    #A or G
-    'Y' : 'CT',	#C or T
-    'S' : 'GC',	#G or C
-    'W' : 'AT',	#A or T
-    'K' : 'GT',	#G or T
-    'M' : 'AC',	#A or C
-    'B' : 'CGT',	#C or G or T
-    'D' : 'AGT',	#A or G or T
-    'H' : 'ACT',	#A or C or T
-    'V' : 'ACG',	#A or C or G
-    'N' : 'ACGTURYSWKMBDHVN',	#any base
-    '.' : '.-',    #gap
-    '-' : '.-'     #gap
+    'A': 'A',  # Adenine
+    'C': 'C',  # Cytosine
+    'G': 'G',  # Guanine
+    'T': 'TU',  # Thymine
+    'U': 'TU',  # Uracil
+    'R': 'AG',  # A or G
+    'Y': 'CT',  # C or T
+    'S': 'GC',  # G or C
+    'W': 'AT',  # A or T
+    'K': 'GT',  # G or T
+    'M': 'AC',  # A or C
+    'B': 'CGT',  # C or G or T
+    'D': 'AGT',  # A or G or T
+    'H': 'ACT',  # A or C or T
+    'V': 'ACG',  # A or C or G
+    'N': 'ACGTURYSWKMBDHVN',  # any base
+    '.': '.-',  # gap
+    '-': '.-'  # gap
 }
+
 
 def IUPACMatch(code1: str, code2: str) -> bool:
     """
@@ -33,6 +44,7 @@ def IUPACMatch(code1: str, code2: str) -> bool:
     :return: True if the codes are within the specified range, False otherwise
     """
     return code2 in IUPACCodeDict[code1]
+
 
 def openGZ(path: str, mode: str):
     """
@@ -46,6 +58,7 @@ def openGZ(path: str, mode: str):
         fh = gzip.open(fh)
     return fh
 
+
 def createAndOpen(path: str, mode: str):
     """
     Open a file, creating the full directory path to the file if necessary.
@@ -53,7 +66,7 @@ def createAndOpen(path: str, mode: str):
     :param mode: Mode to open file in (same as open() paramater)
     :return: A open file object, similar to open()
     """
-    if not os.path.exists(os.path.dirname(path)):
+    if os.path.dirname(path) and not os.path.exists(os.path.dirname(path)):
         try:
             os.makedirs(os.path.dirname(path))
         except OSError as e:
@@ -64,9 +77,10 @@ def createAndOpen(path: str, mode: str):
     else:
         return open(path, mode)
 
-@ConfigMap(inStream=TransformCfg('input', (openGZ, None, ('rb',))), mateStream= TransformCfg('mate', (openGZ, None, ('rb',))), outStream=TransformCfg('output', (createAndOpen, None, ('wb+',))), logStream=TransformCfg('log', (createAndOpen, None, ('w+',))))
-@ArgMap(inStream=PositionalArg(0, 'reads.fastq[.gz]', 'Path to fastq with reads to trim.', (openGZ, None, ('wb+',))), mateStream=PositionalArg(1, 'mates.fastq[.gz]', 'Mates fastq that will also be trimmed and interlaced in the output.', (openGZ, None, ('wb+',))), outStream=PositionalArg(2, 'output.fastq[.gz]', 'Path to output trimmed fastq to.', (createAndOpen, None, ('wb+',))), logStream=PositionalArg(3, 'log', 'Path to output verbose log.', (createAndOpen, None, ('wb+',))))
-def trim(barcode_distance: int, barcode_sequence: str, reverse: bool = False, inStream: io.IOBase = stdin, outStream: io.IOBase = stdout, mateStream: io.IOBase = None, verbose: bool = False, logStream: io.IOBase=stderr) -> (int, int):
+
+@ConfigMap(inStream=TransformCfg('input', (openGZ, None, ('rb',))), mateStream=TransformCfg('mate', (openGZ, None, ('rb',))), outStream=TransformCfg('output', (createAndOpen, None, ('wb+',))), logStream=TransformCfg('log', (createAndOpen, None, ('w+',))))
+@ArgMap(inStream=PositionalArg(0, 'reads.fastq[.gz]', 'Path to fastq with reads to trim.', (openGZ, None, ('rb+',))), mateStream=PositionalArg(1, 'mates.fastq[.gz]', 'Mates fastq that will also be trimmed and interlaced in the output.', (openGZ, None, ('rb+',))), outStream=PositionalArg(2, 'output.fastq[.gz]', 'Path to output trimmed fastq to.', (createAndOpen, None, ('wb+',))), logStream=PositionalArg(3, 'log', 'Path to output verbose log.', (createAndOpen, None, ('wb+',))))
+def trim(barcode_distance: int, barcode_sequence: str, reverse: bool = False, inStream: io.IOBase = stdin, outStream: io.IOBase = stdout, mateStream: io.IOBase = None, verbose: bool = True, logStream: io.IOBase=stderr) -> (int, int):
     """
     Trims barcodes from reads in a fastq file
     :param inStream: A file or stream handle to read input data
@@ -78,22 +92,26 @@ def trim(barcode_distance: int, barcode_sequence: str, reverse: bool = False, in
     :param verbose: Provide verbose output while processing
     :return:
     """
+
+    printPrefix = "PRODUSE-TRIM"
     if verbose:
-        logStream.write("\n") # This will be deleted by the next write
+        logStream.write("\t".join([printPrefix, time.strftime('%X'), "Starting...\n"]))
     record1 = FastqRecord.FastqRecord()
     mated = mateStream is not None
     if mated:
         record2 = FastqRecord.FastqRecord()
     invert = False
-    count = 0 # Record counter
-    discard = 0 # Discarded records counter
+    count = 0  # Record counter
+    discard = 0  # Discarded records counter
     if barcode_distance < 0:
         invert = True
         barcode_distance *= -1
 
     while record1.read(inStream) and (not mated or record2.read(mateStream)):
+        """
         if verbose:
             logStream.write("\x1b[F\x1b[2K\r{file}\tWorking on record: {record}\tRecords processed: {total}\n".format(file=outStream.name if hasattr(outStream, 'name') else 'Streaming', record=record1.name, total=count))
+        """
         count += 1
         # Store barcode in sequence description
         barcode1 = (record1.seq[-len(barcode_sequence):] if reverse else record1.seq[:len(barcode_sequence)])
@@ -110,7 +128,7 @@ def trim(barcode_distance: int, barcode_sequence: str, reverse: bool = False, in
             mismatch += sum(not IUPACMatch(bc, sq) for bc, sq in zip(barcode_sequence, reversed(barcode) if reverse else barcode))
             if len(barcode) < len(barcode_sequence):
                 mismatch += len(barcode_sequence) - len(barcode)
-            if invert != (mismatch > barcode_distance): #XOR
+            if invert != (mismatch > barcode_distance):  # XOR
                 discard += 1
                 # Keep empty record to make BWA happy about having read mate
                 record.seq = ''
@@ -119,14 +137,21 @@ def trim(barcode_distance: int, barcode_sequence: str, reverse: bool = False, in
                 record.trim(0 if reverse else len(barcode_sequence), len(barcode_sequence) if reverse else 0)
             record.write(outStream)
 
+            # Provide status updates
+        if verbose and count % 1000 == 0:
+            if mated:
+                logStream.write("\t".join([printPrefix, time.strftime('%X'), "Read Pairs Trimmed: %s Discard Rate: %s \r" % (count, discard / count)]))
+            else:
+                logStream.write("\t".join([printPrefix, time.strftime('%X'), "Reads Trimmed: %s Discard Rate: %s \r" % (count, discard / count)]))
+
     if verbose:
-        logStream.write("\x1b[F\x1b[2K\r{file}\tTotal records: {total}\tRecords discarded: {discard}\n".format(file=outStream.name if hasattr(outStream, 'name') else 'Streaming', total=count, discard=discard))
+        logStream.write("\t".join(["\n" + printPrefix, time.strftime('%X'), "Trimming Complete\n"]))
     outStream.close()
     return discard, count
 
+
 if __name__ == "__main__":
     from sys import stdout, stdin, argv
-    import os, errno
     from configutator import loadConfig
 
     cfgs = loadConfig(argv, (trim,), title="Trim V1.0")
@@ -135,7 +160,7 @@ if __name__ == "__main__":
             argmap = next(cfgs)
             trim(**argmap[trim])
         except ValueError:
-            continue # TODO add verbose output
+            continue  # TODO add verbose output
         except StopIteration:
             break
 
